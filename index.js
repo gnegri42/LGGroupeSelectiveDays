c6de2c6e91c09ee531f6cd00e84318f245a98605
Geoffrey Negri <contact@geoffreynegri.fr>
2022-06-27 17:57:34 +0200

    Added a way to send test mails with Mailtrap, and another methdod to send mail with Sendgrid. Sendgrid API key must be in a sendgrid.env file

--- index.js
+++ index.js
@@ -1,78 +1,133 @@
 const rp = require('request-promise-native');
 const fs = require('fs');
 const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
+
+function sendMail(vehicles) {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const vehiclesList = vehicles.toString();
+
+  const msg = {
+    to: 'dev@agenceartemis.fr',
+    from: 'dev@agenceartemis.fr',
+    subject: 'Vehicules vendus',
+    text: vehiclesList,
+    html: vehiclesList,
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+
+// SENDING MAILS WITH NODEMAILER AND MAILTRAP AS TEST
+// function sendMail(vehicles) {
+//   var transport = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "",
+//       pass: ""
+//     }
+//   });
+
+//   const vehiclesList = vehicles.toString();
+
+//   message = {
+//     from: "from-example@email.com",
+//     to: "to-example@email.com",
+//     subject: "Subject",
+//     html: vehiclesList
+//   };
+
+//   transport.sendMail(message, (err, info) => {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       console.log(info);
+//     }
+//   })
+// }
 
 const urls = [
-    'https://lggroupe.com/vehicule/?vehicule=327882970244',
-    'https://lggroupe.com/vehicule/?vehicule=355188040244',
-    'https://lggroupe.com/vehicule/?vehicule=357601410244',
-    'https://lggroupe.com/vehicule/?vehicule=349711360244',
-    'https://lggroupe.com/vehicule/?vehicule=350040000244',
-    'https://lggroupe.com/vehicule/?vehicule=353880730244',
-    'https://lggroupe.com/vehicule/?vehicule=342737090244',
-    'https://lggroupe.com/vehicule/?vehicule=352523590244',
-    'https://lggroupe.com/vehicule/?vehicule=344089540244',
-    'https://lggroupe.com/vehicule/?vehicule=357601020244',
-    'https://lggroupe.com/vehicule/?vehicule=357601970244',
-    'https://lggroupe.com/vehicule/?vehicule=355651360244',
-    'https://lggroupe.com/vehicule/?vehicule=357602750244',
-    'https://lggroupe.com/vehicule/?vehicule=357602250244',
+  'https://lggroupe.com/vehicule/?vehicule=327882970244',
+  'https://lggroupe.com/vehicule/?vehicule=355188040244',
+  'https://lggroupe.com/vehicule/?vehicule=357601410244',
+  'https://lggroupe.com/vehicule/?vehicule=349711360244',
+  'https://lggroupe.com/vehicule/?vehicule=350040000244',
+  'https://lggroupe.com/vehicule/?vehicule=353880730244',
+  'https://lggroupe.com/vehicule/?vehicule=342737090244',
+  'https://lggroupe.com/vehicule/?vehicule=352523590244',
+  'https://lggroupe.com/vehicule/?vehicule=344089540244',
+  'https://lggroupe.com/vehicule/?vehicule=357601020244',
+  'https://lggroupe.com/vehicule/?vehicule=357601970244',
+  'https://lggroupe.com/vehicule/?vehicule=355651360244',
+  'https://lggroupe.com/vehicule/?vehicule=357602750244',
+  'https://lggroupe.com/vehicule/?vehicule=357602250244',
+  // 'https://lggroupe.com/vehicule/?vehicule=322672930244',
+  'https://lggroupe.com/vehicule/?vehicule=335824000244',
 ]
 
 const sleep = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 async function downloadHtml(uri) {
-    // the output filename
-    const filename = 'result.html';
+  // the output filename
+  const filename = 'result.html';
 
-    // check if we already have the file
-    // const fileExists = fs.existsSync(filename);
-    // if (fileExists) {
-    //     console.log(`Skipping download for ${uri} since ${filename} already exists.`);
-    // return;
-    // }
+  // check if we already have the file
+  // const fileExists = fs.existsSync(filename);
+  // if (fileExists) {
+  //     console.log(`Skipping download for ${uri} since ${filename} already exists.`);
+  // return;
+  // }
 
-    // download the HTML from the web server
-    // console.log(`Downloading HTML from ${uri}...`);
-    const results = await rp({ uri: uri });
-    // save the HTML to disk
-    await fs.promises.writeFile(filename, results);
+  // download the HTML from the web server
+  // console.log(`Downloading HTML from ${uri}...`);
+  const results = await rp({ uri: uri });
+  // save the HTML to disk
+  await fs.promises.writeFile(filename, results);
 }
 
 async function parsePage() {
-    // console.log('Parsing box score HTML...');
-    // the input filename
-    const htmlFilename = 'result.html';
-    // read the HTML from disk
-    const html = await fs.promises.readFile(htmlFilename);
-    // parse the HTML with Cheerio
-    const $ = cheerio.load(html);
-    // Get our rows
-    const $trs = $('.container-vehicule');
-    // console.log($.html($trs));
-    // console.log("TRS :" + $trs);
-    if($trs == "")
-        return sleep(300).then(v => false);
-    return sleep(300).then(v => true);
-  }
+  // console.log('Parsing box score HTML...');
+  // the input filename
+  const htmlFilename = 'result.html';
+  // read the HTML from disk
+  const html = await fs.promises.readFile(htmlFilename);
+  // parse the HTML with Cheerio
+  const $ = cheerio.load(html);
+  // Get our rows
+  const $trs = $('.container-vehicule');
+  // console.log($.html($trs));
+  // console.log("TRS :" + $trs);
+  if ($trs == "")
+    return sleep(100).then(v => false);
+  return sleep(100).then(v => true);
+}
 
-  async function main() {
-    for (let index = 0; index < urls.length; index++) {
-        const uri = urls[index];
-        await downloadHtml(uri);
-        // console.log('Starting...');
-        const boxScore = await parsePage();
-        console.log(boxScore + " : " + uri);
-    }
-    // save the scraped results to disk
-    // await fs.promises.writeFile(
-    //   'boxscore.json',
-    //   JSON.stringify(resultJSON)
-    // );
-
-    console.log('Done!');
+async function main() {
+  var offlineVehicles = [];
+  for (let index = 0; index < urls.length; index++) {
+    const uri = urls[index];
+    await downloadHtml(uri);
+    // console.log('Starting...');
+    const vehicleState = await parsePage();
+    console.log(vehicleState + " : " + uri);
+    if (!vehicleState)
+      offlineVehicles.push(uri);
   }
-  
-  main();
+  if (offlineVehicles.length != 0)
+    sendMail(offlineVehicles);
+  console.log('Done!');
+}
+
+main();
